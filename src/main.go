6072dff08c1b6b69c9144bea45028a7f8285537b
Jim Sloan <jsloan@mchs.com>
2018-10-15 17:37:25 -0400

    more work on building the data structure, sorting, and reporting

--- src/main.go
+++ src/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
@@ -17,6 +18,13 @@ func check(e error) {
 	}
 }
 
+type meeting struct {
+	subject    string
+	start, end time.Time
+	show       int
+	allday     bool
+}
+
 func main() {
 
 	// the csv file
@@ -25,13 +33,18 @@ func main() {
 	f, _ := os.Open(inputfile)
 
 	// date format
-	layout := "1/2/2006 3:04:05 PM"
+	dateformat := "1/2/2006 3:04:05 PM"
+	timeformat := "3:04PM"
+	defaultstart := "6:00:00 AM"
+	defaultend := "3:00:00 PM"
 
 	// counting lines
 	lines := 0
 
 	// create a new reader.
 	r := csv.NewReader(bufio.NewReader(f))
+
+	m := make(map[string][]meeting)
 
 	// read and process
 	for {
@@ -60,18 +73,52 @@ func main() {
 		check(err)
 
 		// convert time to unixtime
-		ustart, err := time.Parse(layout, startdate+" "+starttime)
+		ustart, err := time.Parse(dateformat, startdate+" "+starttime)
 		check(err)
 
-		uend, err := time.Parse(layout, enddate+" "+endtime)
+		uend, err := time.Parse(dateformat, enddate+" "+endtime)
 		check(err)
 
-		// display results
 		if showas == 2 {
-			fmt.Printf("%v\n", subject)
-			fmt.Printf("%v ALLDAY:%t SHOWAS:%v\n", startdate, allday, showas)
-			fmt.Printf("%d - %d = %d\n\n", ustart.Unix(), uend.Unix(), (uend.Unix()-ustart.Unix())/60)
+			m[startdate] = append(m[startdate], meeting{
+				subject: subject,
+				start:   ustart,
+				end:     uend,
+				allday:  allday,
+				show:    showas,
+			})
 		}
+	}
 
+	// sort the days
+	days := make([]string, 0, len(m))
+	for k := range m {
+		days = append(days, k)
+	}
+	sort.Strings(days)
+
+	// loop over the sorted days
+	for d := range days {
+		day := m[days[d]]
+
+		daystart, err := time.Parse(dateformat, days[d]+" "+defaultstart)
+		check(err)
+		dayend, err := time.Parse(dateformat, days[d]+" "+defaultend)
+		check(err)
+
+		fmt.Printf("%v (%v - %v)\n", days[d], daystart.Format(timeformat), dayend.Format(timeformat))
+
+		// sort meetings by start time
+		sort.Slice(day, func(i, j int) bool {
+			return day[i].start.Unix() < day[j].start.Unix()
+		})
+
+		// report on each day
+		daytotal := 0
+		for i := range day {
+			daytotal = daytotal + int((day[i].end.Unix() - day[i].start.Unix()))
+			fmt.Printf("\t%v\n\t\t%v - %v = %d\n", day[i].subject, day[i].start.Format("3:04PM"), day[i].end.Format("3:04PM"), (day[i].end.Unix()-day[i].start.Unix())/60)
+		}
+		fmt.Printf("====================================\n\ttotal = %d\n\n", daytotal/60)
 	}
 }
